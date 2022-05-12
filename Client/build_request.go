34f74966857bd3cf7d43c573042457acb7b7767d
Michel Krispin <michel.krispin@gmail.com>
2022-05-12 19:27:48 +0200

    Added crypto utilities and test keys

--- Client/build_request.go
+++ Client/build_request.go
@@ -7,7 +7,7 @@ import (
 func buildRequest(innerBody string, addresses []string) string {
 	request := innerBody
 	l := len(addresses)
-	for i, _ := range addresses {
+	for i := range addresses {
 		idx := l - i - 1 // Wrap from back to front
 		addressBytes := make([]byte, 4)
 		binary.BigEndian.PutUint32(addressBytes[0:], uint32(len(addresses[idx])))
