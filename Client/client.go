ca038ef8eb0e43c00e7f5f856e1ed55bc2666060
Michel Krispin <michel.krispin@gmail.com>
2022-05-12 17:52:29 +0200

    Improved printing

--- Client/client.go
+++ Client/client.go
@@ -11,6 +11,18 @@ func checkError(err error) {
 	}
 }
 
+func printRoute(firstHop string, hops []string) {
+	result := "The route goes\n\t" + firstHop + " -> "
+	l := len(hops) - 1
+	for i, _ := range hops {
+		result += hops[l-i]
+		if i != l {
+			result += " -> "
+		}
+	}
+	log.Println(result)
+}
+
 const serverRequest = ("GET / HTTP/1.1\r\n" +
 	"Host: 127.0.0.1:8080\r\n\r\n")
 
@@ -20,62 +32,24 @@ func main() {
 	// Create the HTTP request
 	// Address go in reverse order
 	// First hop should no be in here
+	firstAddress := "127.0.0.1:8002"
 	addresses := []string{
 		"127.0.0.1:8080", // First one is the server
-		"127.0.0.1:8000",
 		"127.0.0.1:8001",
+		"127.0.0.1:8000",
 	}
-
 	request := buildRequest(serverRequest, addresses)
 
+	printRoute(firstAddress, addresses)
+
 	// Send that request into the socket
-	firstAddress := "127.0.0.1:8002"
 	c, err := net.Dial("tcp", firstAddress)
 	checkError(err)
 	log.Println("Connected to " + firstAddress + " socket")
 
 	c.Write([]byte(request))
-	log.Println("Wrote request\n" + request)
 
 	// Read the return
 	_, response := parseRequest(c)
-	/*
-		buf := bufio.NewReader(c)
-		response := ""
-		byteCounter := 0
-		foundLength := false
-		contentLength := 0
-		contentLengthStr := ""
-		foundBody := false
-		for {
-			b, err := buf.ReadByte()
-			if err != nil {
-				if err.Error() != "EOF" {
-					log.Fatalln(err)
-				}
-				break
-			}
-			if !foundLength && strings.Contains(response, "Content-Length: ") {
-				if b == '\r' {
-					foundLength = true
-					contentLength, err = strconv.Atoi(contentLengthStr)
-					checkError(err)
-				} else {
-					contentLengthStr += string(b)
-				}
-			} else if !foundBody && strings.Contains(response, "\r\n\r\n") {
-				foundBody = true
-			}
-			response += string(b)
-			if foundBody {
-				if byteCounter >= contentLength-1 {
-					c.Close()
-					break
-				}
-				byteCounter++
-			}
-		}
-	*/
-
 	log.Println("Received:\n", response)
 }
