675b0916b7c1028e626feed067c1c0eb9aa5957b
Michel Krispin <michel.krispin@gmail.com>
2022-05-13 21:17:10 +0200

    Added simple HTML template

--- DirectoryNode/directory_node.go
+++ DirectoryNode/directory_node.go
@@ -13,8 +13,10 @@ var connectionsSend []connection
 
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
 	router.GET("/", func(c *gin.Context) {
-		c.IndentedJSON(http.StatusOK, gin.H{
+		c.HTML(http.StatusOK, "index.html", gin.H{
 			"registered": nodes,
 			"received":   connectionsReceived,
 			"send":       connectionsSend,
