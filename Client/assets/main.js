cfde2ebc9472374944fb047d229b0cd425f66b2c
MichelKrispin <michel.krispin@gmail.com>
2022-06-01 17:39:35 +0200

    Added base for dashboard

--- Client/assets/main.js
+++ Client/assets/main.js
@@ -1,26 +1,84 @@
+// =======================
+// Functions
+// =======================
+
+const createBox = (title, x, y, color, borderColor) => {
+  let directoryRect = two.makeRectangle(x, y, 100, 50);
+  directoryRect.fill = color;
+  directoryRect.stroke = borderColor;
+  two.makeText(title, x, y);
+};
+
+// =======================
+// Initialization
+// =======================
+
 $("#loader").hide();
 
-var params = {
-  fullscreen: false,
-};
-var elem = document.getElementById("dashboard");
-var two = new Two(params).appendTo(elem);
+// =======================
+// Drawing
+// =======================
+
+let dashboard = document.getElementById("dashboard");
+const width = dashboard.offsetWidth;
+const height = 600;
+
+let two = new Two({
+  width: width,
+  height: height,
+}).appendTo(dashboard);
 
 // Create the client box
-let clientX = 110;
-let clientY = 100;
-let clientRect = two.makeRectangle(clientX, clientY, 100, 50);
-clientRect.fill = "rgba(0, 200, 255, 0.75)";
-clientRect.stroke = "#1C75BC";
-let clientText = two.makeText("Client", clientX, clientY);
+createBox(
+  "Client",
+  width * 0.1,
+  height * 0.4,
+  "rgba(0, 200, 255, 0.75)",
+  "#1C75BC"
+);
 
 // Create the directory box
-let directoryX = 500;
-let directoryY = 50;
-let directoryRect = two.makeRectangle(directoryX, directoryY, 100, 50);
-directoryRect.fill = "rgba(0, 200, 255, 0.75)";
-directoryRect.stroke = "#1C75BC";
-let directoryText = two.makeText("Directory", directoryX, directoryY);
+createBox(
+  "Directory",
+  width * 0.5,
+  height * 0.1,
+  "rgba(200, 0, 255, 0.75)",
+  "#751CBC"
+);
+
+// Create the nodes
+createBox(
+  "Node 1",
+  width * 0.8,
+  height * 0.3,
+  "rgba(200, 0, 255, 0.75)",
+  "#751CBC"
+);
+
+createBox(
+  "Node 2",
+  width * 0.8,
+  height * 0.5,
+  "rgba(200, 0, 255, 0.75)",
+  "#751CBC"
+);
+
+createBox(
+  "Node 2",
+  width * 0.8,
+  height * 0.7,
+  "rgba(200, 0, 255, 0.75)",
+  "#751CBC"
+);
+
+// Create the service box
+createBox(
+  "Service",
+  width * 0.5,
+  height * 0.9,
+  "rgba(200, 0, 255, 0.75)",
+  "#751CBC"
+);
 
 // Draw everything to the screen
 two.update();
